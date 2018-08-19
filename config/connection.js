4c13f79ca2b028f475fb85b5cf18b84ebf64f366
annette.beatty@gmail.com <annette.beatty@gmail.com>
2018-08-19 07:16:33 -0700

    Most testing

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,15 @@ var mysql = require("mysql");
 
 var connection;
 
+var jaws_stuff = "mysql://xu3i6a0c8eq8459q:hswev4hnf9n4u3ur@blonze2d5mrbmcgf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/q44rsks4taxotx55";
+
 console.log("In connection ", process.env.JAWSDB_URL);
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+if (jaws_stuff)
+{
+  connection = mysql.createConnection(jaws_stuff);
+//if (process.env.JAWSDB_URL) {
+//  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   //local host
     connection = mysql.createConnection({
